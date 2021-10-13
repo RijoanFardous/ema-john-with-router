029b703c60fedb7ff1acd85baaf69a353d2783e0
RijoanFardous <rijoan23@gmail.com>
2021-10-13 11:30:52 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/orders">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <PageNotFound></PageNotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
